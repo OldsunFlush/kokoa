ddbe6bbc17a8a3d338ee143121d1284035e42e01
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-06-24 17:43:52 -0400

    Update data provider

--- src/routes/symbol/[symbol]/+page.ts
+++ src/routes/symbol/[symbol]/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from './$types';
-import { PUBLIC_ALPHAVANTAGE_API } from '$env/static/public';
+import { PUBLIC_POLYGONIO_API } from '$env/static/public';
 
 export const load = (async ({ params, fetch }) => {
 	const res = await fetch(
-		`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${params.symbol}&apikey=${PUBLIC_ALPHAVANTAGE_API}`,
+		`https://api.polygon.io/v3/reference/tickers/${params.symbol.toUpperCase()}?apiKey=${PUBLIC_POLYGONIO_API}`,
 		{
 			// headers: {
 			// 	Authorization: `Bearer ${"demo"}`,
@@ -12,17 +12,21 @@ export const load = (async ({ params, fetch }) => {
 		}
 	);
 	const item = await res.json();
+	const stock = item.results;
+	console.log('🚀 ~ load ~ item:', item);
 	return {
-		country: item.Country,
-		currency: item.Currency,
-		description: item.Description,
-		dividend: item.DividendPerShare,
-		exchange: item.Exchange,
-		industry: item.Industry,
-		marketCap: item.MarketCapitalization,
-		name: item.Name,
-		sector: item.Sector,
-		symbol: item.Symbol,
-		type: item.AssetType
+		branding: {
+			logo: stock.branding.logo_url,
+			icon: stock.branding.icon_url
+		},
+		currency: stock.currency_name.toUpperCase(),
+		description: stock.description,
+		country: stock.locale.toUpperCase(),
+		exchange: stock.primary_exchange,
+		marketCap: stock.market_cap,
+		name: stock.name,
+		sector: stock.sic_description,
+		symbol: stock.ticker,
+		type: stock.type,
 	};
 }) satisfies PageLoad;
